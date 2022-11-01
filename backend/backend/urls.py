90cbe32ddf6b8254d3e2fa1b5b58a85bb1c2a01a
rodom_macbook <enjoying1018@naver.com>
2022-11-02 04:24:10 +0900

    location2 url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,6 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
-    path('location/', include('location.urls')),
+    path('location2/', include('location.urls')),
 
 ]
